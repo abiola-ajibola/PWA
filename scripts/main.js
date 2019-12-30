a11bb2544aed4045606863efd86ff152544d60d9
abiola-ajibola <ajibolaabiolaahmed@gmail.com>
2019-12-30 16:12:42 +0100

    A simple Progressive Web App

---
+++ scripts/main.js
@@ -0,0 +1,86 @@
+window.onload = () => {
+    if('serviceWorker' in navigator){
+        navigator.serviceWorker.register('/serviceWorker.js');
+    }
+
+    /* *****************************Image slider**************************************** */
+
+    /* *************************Declare/Initialise variables**************************** */
+    const image = document.querySelectorAll('.img-container');
+    const nxt = document.querySelector('.next-btn'),
+        prv = document.querySelector('.previous-btn'),
+        dots = document.querySelectorAll('.dots'),
+        dotsArray = [...dots];
+    let i = 0, inter;
+
+    /* ****************Toggles the background of each dot accordingly****************** */
+    const dotsFunction = (index) => {
+        dots.forEach((dot) => {
+            dot.classList.remove('active');
+        })
+        dots[index].classList.add('active');
+    }
+
+    /* *************************Makes an image to be visible************************ */
+    const show = (index) => {
+        image[index].classList.add('show');
+    }
+
+    /* Brings up the next image */
+    const next = () => {
+        image.forEach((img) => {
+            img.classList.remove('show');
+        })
+        i++;
+        if (i >= image.length) i = 0;
+        show(i);
+        dotsFunction(i);
+    }
+
+    /* Brings up the previous image */
+    const previous = () => {
+        image.forEach((img) => {
+            img.classList.remove('show');
+        })
+        i--;
+        if (i < 0) (i = image.length - 1);
+        show(i);
+        dotsFunction(i);
+    }
+
+    /* Brings up the next image after 4 seconds */
+    const interval = setInterval(next, 4000);
+
+    /* Resets the timer on each call */
+    const timing = () => {
+        clearInterval(interval);
+        clearInterval(inter);
+        inter = setInterval(next, 4000);
+    }
+
+    image[i].classList.add('show'); // Displays the first image since i = 0
+    dots[i].classList.add('active');
+
+    /* Fires the onclick event on clicking the next(nxt) or previous(prv) divs div */
+    nxt.onclick = () => {
+        next();// calls the next function
+        timing(); // calls the timing function to reset the timer
+    }
+
+    prv.onclick = () => {
+        previous(); // calls the previous function
+        timing();
+    }
+
+    /* on clicking any of the dots, run the dotsFunction for the clicked dot
+     and show the corresponding image for the dot */
+    dots.forEach((dot) => {
+        dot.onclick = function () {
+            i = (dotsArray.indexOf(dot));
+            dotsFunction(i);
+            image.forEach((img) => img.classList.remove('show'));
+            timing();
+            show(i);
+        }
+    })
+}
---
+++ serviceWorker.js
@@ -0,0 +1,44 @@
+// import update from './version.js';
+let cacheName = 'sample',
+    resources = ['/','manifest.json', 'scripts/main.js', 'styles/style.css', 'images/Android-icon512.png',
+        'images/catplainicon_97046_256.png', 'images/catplainicon_97046(1)128.png', 'images/catplainicon_97046(1)144.png',
+        'images/catplainicon_97046(1)168.png', 'images/catplainicon_97046(1)192.png', 'images/catplainicon_97046(2)96.png',
+        'images/catplainicon_97046(3)72.png', 'images/catplainicon_97046(4)64.png', 'images/catplainicon_97046(5)48.png',
+        'images/daniel-mirlea-_7IUgAL60nc-unsplash.jpg', 'images/eberhard-grossgasteiger-TMHL7wald8I-unsplash.jpg',
+        'images/martin-schmidli-mGy1Jjr2e6M-unsplash.jpg', 'images/raquel-garcia-BAiSZDx4LgE-unsplash.jpg',
+        'images/willian-justen-de-vasconcellos-lzOzsGmAg3s-unsplash.jpg'];
+
+self.addEventListener('install', function (e) {
+        e.waitUntil(
+            caches.open(cacheName).then((cache) => {
+                return cache.addAll(resources);
+            })
+        );
+    console.log('Service worker installed')
+    });
+
+self.addEventListener('activate', function () {
+    clients.claim();
+    console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(mat => {
+            return mat || fetch(e.request);
+        })
+    )
+
+    e.waitUntil(
+        async function() {
+            const cache = await caches.open(cacheName);
+            const networkRes = await fetch(e.request);
+            await cache.put(e.request, networkRes.clone())
+        }
+        // caches.open(cacheName).then(fetch(e.request))
+        // .then(cache => cache.put(e.request, response.clone()))
+    )
+
+    console.log('Assets retrieved')
+    // console.log(update)
+});
