4b31bb6446959bfe139504b331750b7c73ede382
abiola-ajibola <ajibolaabiolaahmed@gmail.com>
2020-01-04 15:34:47 +0100

    Create separate caches for images and other files

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,43 +1,44 @@
-let cacheName = 'sample',
-    resources = ['./','./manifest.json', './scripts/main.js', './styles/style.css', './images/Android-icon512.png',
-        './images/catplainicon_97046_256.png', './images/catplainicon_97046(1)128.png', './images/catplainicon_97046(1)144.png',
-        './images/catplainicon_97046(1)168.png', './images/catplainicon_97046(1)192.png', './images/catplainicon_97046(2)96.png',
-        './images/catplainicon_97046(3)72.png', './images/catplainicon_97046(4)64.png', './images/catplainicon_97046(5)48.png',
-        './images/daniel-mirlea-_7IUgAL60nc-unsplash.jpg', './images/eberhard-grossgasteiger-TMHL7wald8I-unsplash.jpg',
-        './images/martin-schmidli-mGy1Jjr2e6M-unsplash.jpg', './images/raquel-garcia-BAiSZDx4LgE-unsplash.jpg',
-        './images/willian-justen-de-vasconcellos-lzOzsGmAg3s-unsplash.jpg'];
+const filesCache = 'filesCache',
+    imagesCache = 'imagesCache',
+    imageFiles = ['./images/Android-icon512.png', './images/catplainicon_97046_256.png', './images/catplainicon_97046(1)128.png',
+        './images/catplainicon_97046(1)144.png', './images/catplainicon_97046(1)168.png', './images/catplainicon_97046(1)192.png',
+        './images/catplainicon_97046(2)96.png', './images/catplainicon_97046(3)72.png', './images/catplainicon_97046(4)64.png',
+        './images/catplainicon_97046(5)48.png', './images/daniel-mirlea-_7IUgAL60nc-unsplash.jpg',
+        './images/eberhard-grossgasteiger-TMHL7wald8I-unsplash.jpg', './images/martin-schmidli-mGy1Jjr2e6M-unsplash.jpg',
+        './images/raquel-garcia-BAiSZDx4LgE-unsplash.jpg', './images/willian-justen-de-vasconcellos-lzOzsGmAg3s-unsplash.jpg'],
+    resources = ['./', './scripts/main.js', './styles/style.css'],
+    cacheList = [filesCache, imagesCache];
 
-self.addEventListener('install', function (e) {
-        e.waitUntil(
-            caches.open(cacheName).then((cache) => {
-                return cache.addAll(resources);
-            })
-        );
-    console.log('Service worker installed')
-    });
-
-self.addEventListener('activate', function () {
-    clients.claim();
-    console.log('Service worker activated');
-});
-
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(mat => {
-            return mat || fetch(e.request);
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        /* Create separate caches for images and other files */
+        caches.open(filesCache).then(cache => {
+            Promise.all([(cache.addAll(resources)), (caches.open(imagesCache).then(images => images.addAll(imageFiles)))])
+                .then(reso => reso);
         })
     )
+});
 
+self.addEventListener('activate', (e) => {
     e.waitUntil(
-        async function() {
-            const cache = await caches.open(cacheName);
-            const networkRes = await fetch(e.request);
-            await cache.put(e.request, networkRes.clone())
-        }
-        // caches.open(cacheName).then(fetch(e.request))
-        // .then(cache => cache.put(e.request, response.clone()))
-    )
+        /* Delete previous versions of caches */
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    );
+})
 
-    console.log('Assets retrieved')
-    // console.log(update)
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        /* Fetch data from cache or get them from network */
+        caches.match(e.request).then(mat => {
+            return mat || fetch(e.request); 
+        })
+    )
 });
